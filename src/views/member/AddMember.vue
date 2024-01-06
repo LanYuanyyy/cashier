ba5a2e508a1156ca657cd006bced8e5b60ed890b
yuan <825293628@qq.com>
2024-01-06 11:49:32 +0800

    新增会员完成

--- src/views/member/AddMember.vue
+++ src/views/member/AddMember.vue
@@ -1,28 +1,58 @@
 <script setup>
-import { ref } from 'vue'
-import { ElMessageBox } from 'element-plus'
-const dialogVisible = ref(false)
+import { ref, reactive } from 'vue'
+import Axios from '../../utils/axios'
+import { ElMessage, ElMessageBox } from 'element-plus'
+const dialogVisible = ref(true)
+const formAdduser = reactive({ name: '', phone: '', pay_type: '', deposit: '', start_time: '', end_time: '' })
+const dateTime = ref([])
 const open = () => {
   dialogVisible.value = true
 }
-defineExpose({
-  open
-})
+const changeTimeDate = (val) => {
+  formAdduser.start_time = val[0]
+  formAdduser.end_time = val[1]
+}
+const emit = defineEmits(['on-update'])
+const addUserApi = async () => {
+  const res = await Axios.post('cashier/user_vip', formAdduser)
+  if (res.code === 200) {
+    dialogVisible.value = false
+    ElMessage({ type: 'success', message: '添加成功' })
+    emit('on-update')
+  } else {
+    ElMessage({ type: 'danger', message: res.message })
+  }
+}
+defineExpose({ open })
 </script>
 
 <template>
-  <el-dialog
-    v-model="dialogVisible"
-    title="Tips"
-    width="30%"
-  >
-    <span>This is a message</span>
+  <el-dialog v-model="dialogVisible" title="新增会员" width="35%">
+    <el-form label-position="left" label-width="100px" :model="formAdduser" style="max-width: 460px">
+      <el-form-item label="用户名">
+        <el-input v-model="formAdduser.name" />
+      </el-form-item>
+      <el-form-item label="手机号">
+        <el-input v-model="formAdduser.phone" />
+      </el-form-item>
+      <el-form-item label="押金">
+        <el-input v-model="formAdduser.deposit" />
+      </el-form-item>
+      <el-form-item label="时间">
+        <el-date-picker v-model="dateTime" type="daterange" range-separator="To" start-placeholder="Start date" end-placeholder="End date" size="large" value-format="YYYY-MM-DD" @change="changeTimeDate" />
+      </el-form-item>
+      <el-form-item label="用户名">
+        <el-radio-group v-model="formAdduser.pay_type">
+          <el-radio :label="1">现金</el-radio>
+          <el-radio :label="2">微信</el-radio>
+          <el-radio :label="3">支付宝</el-radio>
+        </el-radio-group>
+      </el-form-item>
+    </el-form>
     <template #footer>
       <span class="dialog-footer">
         <el-button @click="dialogVisible = false">Cancel</el-button>
-        <el-button type="primary" @click="dialogVisible = false">
-          Confirm
-        </el-button>
+        <el-button type="primary" @click="addUserApi"> Confirm </el-button>
       </span>
     </template>
   </el-dialog>
--- src/views/member/member.vue
+++ src/views/member/member.vue
@@ -2,13 +2,15 @@
 import { ref, onMounted, reactive } from 'vue'
 import Axios from '../../utils/axios.js'
 import { ElMessage, ElMessageBox } from 'element-plus'
-import  AddMember  from './AddMember.vue'
+import AddMember from './AddMember.vue'
 const total = ref(0)
 const tableData = ref([])
 const user_vip = ref({})
 const addUserMemberRef = ref(null)
 const formInline = reactive({ name: '', phone: '' })
-const onSubmit = () => { getMemberList() }
+const onSubmit = () => {
+  getMemberList()
+}
 const cleanUp = () => {
   formInline.name = ''
   formInline.phone = ''
@@ -32,7 +34,6 @@ const renewApi = async (id, value) => {
   } else {
     // danger
     ElMessage({ type: 'danger', message: res.msg })
-
   }
 }
 const modifyPhoneApi = async (id, value) => {
@@ -57,34 +58,44 @@ const renew = (id) => {
     confirmButtonText: 'OK',
     cancelButtonText: 'Cancel',
   })
-    .then(({ value }) => { renewApi(id, value) })
-    .catch(() => { ElMessage({ type: 'info', message: '取消续费' }) })
+    .then(({ value }) => {
+      renewApi(id, value)
+    })
+    .catch(() => {
+      ElMessage({ type: 'info', message: '取消续费' })
+    })
 }
 const modifyPhone = (id) => {
   ElMessageBox.prompt('请输入新的手机号码', '会员续费', {
     confirmButtonText: 'OK',
     cancelButtonText: 'Cancel',
     inputPattern: /^1[3456789]\d{9}$/,
-    inputErrorMessage: 'Invalid Phone'
+    inputErrorMessage: 'Invalid Phone',
   })
-    .then(({ value }) => { modifyPhoneApi(id, value) })
-    .catch(() => { ElMessage({ type: 'info', message: '取消修改' }) })
+    .then(({ value }) => {
+      modifyPhoneApi(id, value)
+    })
+    .catch(() => {
+      ElMessage({ type: 'info', message: '取消修改' })
+    })
 }
-onMounted(() => { getMemberList() })
+onMounted(() => {
+  getMemberList()
+})
 </script>
 
 <template>
   <el-descriptions title="User Info" v-if="user_vip">
-    <el-descriptions-item label="Username">{{user_vip.name}}</el-descriptions-item>
-    <el-descriptions-item label="Telephone">{{user_vip.phone}}</el-descriptions-item>
-    <el-descriptions-item label="Date 时间">{{user_vip.created_at}}</el-descriptions-item>
+    <el-descriptions-item label="Username">{{ user_vip.name }}</el-descriptions-item>
+    <el-descriptions-item label="Telephone">{{ user_vip.phone }}</el-descriptions-item>
+    <el-descriptions-item label="Date 时间">{{ user_vip.created_at }}</el-descriptions-item>
     <el-descriptions-item label="租借订单数">
-      <el-tag size="small">{{user_vip.hire_count}}</el-tag>
+      <el-tag size="small">{{ user_vip.hire_count }}</el-tag>
     </el-descriptions-item>
-    <el-descriptions-item label="押金" v-if="user_vip.vip">{{user_vip.vip.deposit}}</el-descriptions-item>
+    <el-descriptions-item label="押金" v-if="user_vip.vip">{{ user_vip.vip.deposit }}</el-descriptions-item>
   </el-descriptions>
   <el-form :inline="true" :model="formInline" class="demo-form-inline">
-    <el-button type="success" @click="addUserMember" style="margin-right:20px">新增会员 +</el-button>
+    <el-button type="success" @click="addUserMember" style="margin-right: 20px">新增会员 +</el-button>
     <el-form-item label="会员名-查询">
       <el-input v-model="formInline.name" placeholder="name" clearable />
     </el-form-item>
@@ -97,15 +108,15 @@ onMounted(() => { getMemberList() })
     </el-form-item>
   </el-form>
   <el-table :data="tableData" stripe style="width: 100%" @row-click="rowClick" highlight-current-row>
-    <el-table-column type="index" label="序号" width='80' align='center' />
-    <el-table-column prop="created_at" label="Date 时间" width='200' />
-    <el-table-column prop="created_at" label="到期时间" width='200' />
-    <el-table-column prop="name" label="Name 用户名" width='200' />
-    <el-table-column prop="phone" label="phone 手机号" width='200' />
-    <el-table-column prop="vip.deposit" label="押金" width='200' />
+    <el-table-column type="index" label="序号" width="80" align="center" />
+    <el-table-column prop="created_at" label="Date 时间" width="200" />
+    <el-table-column prop="created_at" label="到期时间" width="200" />
+    <el-table-column prop="name" label="Name 用户名" width="200" />
+    <el-table-column prop="phone" label="phone 手机号" width="200" />
+    <el-table-column prop="vip.deposit" label="押金" width="200" />
     <el-table-column prop="hire_count" label="订单数" />
-    <el-table-column fixed="right" label="Operations" width='250'>
-      <template #default='{row}'>
+    <el-table-column fixed="right" label="Operations" width="250">
+      <template #default="{ row }">
         <el-button link type="primary" size="small" @click="renew(row.id)">续费</el-button>
         <el-button link type="info" size="small" @click="modifyPhone(row.id)">修改手机号</el-button>
         <el-button link type="danger" size="small" @click="detailUser(row.id)">Detail</el-button>
@@ -113,9 +124,8 @@ onMounted(() => { getMemberList() })
     </el-table-column>
   </el-table>
   <el-pagination layout="prev, pager, next" :total="total" @change="handleSizeChange" />
-  <AddMember ref='addUserMemberRef' />
+  <AddMember ref="addUserMemberRef" @on-update="getMemberList" />
 </template>
-
 
 <style lang="scss" scoped>
 .el-form {
